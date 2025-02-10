74019b1049b9dd6701770984a1f1b202f7bf9f8d
alirni <aliramezani19.1@gmail.com>
2025-02-10 20:11:03 +0330

    feat: setup ui directory

--- apps/game-board/src/app/page.tsx
+++ apps/game-board/src/app/page.tsx
@@ -1,8 +1,11 @@
+import { MyUi } from '@game-hub/ui';
+
 export default function Index() {
   return (
     <main className='flex flex-col items-center justify-center h-screen m-4'>
       <h1 className='text-7xl'>GameHub</h1>
       <p className='text-2xl'>Welcome to the GameHub!</p>
+      <MyUi />
     </main>
   );
 }
---
+++ libs/ui/src/lib/ui.tsx
@@ -0,0 +1,9 @@
+export function MyUi() {
+  return (
+    <div className='text-red-500 text-3xl'>
+      <h1>Welcome to Ui!</h1>
+    </div>
+  );
+}
+
+export default MyUi;
