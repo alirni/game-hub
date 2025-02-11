d4aff24937af63eb55fd2be52bbdd76df360b8ab
alirni <aliramezani19.1@gmail.com>
2025-02-11 12:27:34 +0330

    fix: change main page style

--- apps/game-board/src/app/page.tsx
+++ apps/game-board/src/app/page.tsx
@@ -1,16 +1,25 @@
-import { MyUi, Card } from '@game-hub/ui';
+import { Card } from '@game-hub/ui';
 
 export default function Index() {
   return (
-    <main className='flex flex-col items-center justify-center h-screen m-4'>
-      <h1 className='text-7xl'>GameHub</h1>
-      <p className='text-2xl'>Welcome to the GameHub!</p>
-      <MyUi />
-      <Card
-        title='test'
-        description='test test test'
-        imageUrl='/placeholder.png'
-      />
+    <main className='flex flex-col h-full'>
+      {/* header */}
+      <div className='m-4 text-center'>
+        <h1 className='text-7xl'>GameHub</h1>
+        <p className='text-2xl'>Welcome to the GameHub!</p>
+      </div>
+
+      {/* //container for the card */}
+      <div className='grid grid-cols-1 gap-4 sm:grid-cols-1 lg:grid-cols-3 overflow-auto p-4'>
+        {Array.from({ length: 10 }).map((_, index) => (
+          <Card
+            key={index}
+            title={`Card ${index + 1}`}
+            description={`This is a card ${index + 1}`}
+            imageUrl='/placeholder.png'
+          />
+        ))}
+      </div>
     </main>
   );
 }
