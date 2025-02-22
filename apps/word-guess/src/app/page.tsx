242afba0e1f4bf81cca50b22c660644ec649e558
alirni <aliramezani19.1@gmail.com>
2025-02-22 20:14:33 +0330

    fix: create container and install formik and make a form

--- apps/word-guess/src/app/page.tsx
+++ apps/word-guess/src/app/page.tsx
@@ -1,81 +1,12 @@
-
-import { Button } from '@game-hub/libs/ui/button';
-import { 
-  Card, 
-  CardHeader,
-  CardTitle,
-  CardContent
-} from '@game-hub/libs/ui/card';
-import {
-  Select,
-  SelectItem,
-  SelectContent,
-  SelectTrigger,
-  SelectValue,
-} from '@game-hub/libs/ui/select';
+import { SetupContainer } from "../components";
 
 export default function Index() {
   return (
     <main className='flex flex-col items-center justify-center min-h-screen bg-gray-100'>
       <h1 className='text-6xl font-bold'>Word Guess</h1>
       <p className='text-2xl text-gray-500'>Guess the word</p>
-
-      {/* setting of game */}
-      <Card>
-        <CardHeader>
-          <CardTitle className='text-lg'>Setup</CardTitle>
-        </CardHeader>
-        <CardContent>
-          <label className='text-xl'>Difficulty</label>
-          <Select defaultValue='medium'>
-            <SelectTrigger>
-              <SelectValue placeholder='Select difficulty' />
-            </SelectTrigger>
-            <SelectContent>
-              <SelectItem value='easy'>Easy</SelectItem>
-              <SelectItem value='medium'>Medium</SelectItem>
-              <SelectItem value='hard'>Hard</SelectItem>
-            </SelectContent>
-          </Select>
-
-          <label className='text-xl'>Category</label>
-          <Select defaultValue='animals'>
-            <SelectTrigger>
-              <SelectValue placeholder='Select category' />
-            </SelectTrigger>
-            <SelectContent>
-              <SelectItem value='animals'>Animals</SelectItem>
-              <SelectItem value='countries'>Countries</SelectItem>
-              <SelectItem value='programming'>Programming</SelectItem>
-            </SelectContent>
-          </Select>
-
-          <label className='text-xl'>Number of words</label>
-          <Select defaultValue='5'>
-            <SelectTrigger>
-              <SelectValue placeholder='Select number of words' />
-            </SelectTrigger>
-            <SelectContent>
-              <SelectItem value='5'>5</SelectItem>
-              <SelectItem value='10'>10</SelectItem>
-              <SelectItem value='15'>15</SelectItem>
-            </SelectContent>
-          </Select>
-
-          <label className='text-xl'>Time limit</label>
-          <Select defaultValue='60'>
-            <SelectTrigger>
-              <SelectValue placeholder='Select time limit' />
-            </SelectTrigger>
-            <SelectContent>
-              <SelectItem value='30'>30 seconds</SelectItem>
-              <SelectItem value='60'>1 minute</SelectItem>
-              <SelectItem value='90'>1 minute 30 seconds</SelectItem>
-            </SelectContent>
-          </Select>
-        </CardContent>
-        <Button size='lg'>Start</Button>
-      </Card>
+      
+      <SetupContainer />
     </main>
   );
 }
---
+++ apps/word-guess/src/components/SetupContainer/SetupContainer.tsx
@@ -0,0 +1,140 @@
+'use client'
+
+import { FC } from 'react';
+
+import { Button } from '@game-hub/libs/ui/button';
+import {
+  Card,
+  CardHeader,
+  CardTitle,
+  CardContent,
+  CardFooter,
+} from '@game-hub/libs/ui/card';
+import {
+  Select,
+  SelectItem,
+  SelectContent,
+  SelectTrigger,
+  SelectValue,
+} from '@game-hub/libs/ui/select';
+
+import { useFormik } from 'formik';
+
+const SetupContainer: FC = () => {
+
+    const formik = useFormik({
+      initialValues: {
+        difficulty: 'medium',
+        category: 'animals',
+        numberOfWords: '5',
+        timeLimit: '60',
+      },
+      onSubmit: (values) => {
+        console.log('SetupContainer.tsx:33 >> values', { values });
+      },
+    });
+
+  const onCustomChange = (name: string) => (value: unknown) => {
+    formik.handleChange({
+      target: {
+        name,
+        value,
+      },
+    });
+  };
+  
+  return (
+    <Card className='w-96 mt-8'>
+        <form onSubmit={formik.handleSubmit}>
+      <CardHeader>
+        <CardTitle className='text-lg'>Setup</CardTitle>
+      </CardHeader>
+      <CardContent>
+          <div className='mb-4'>
+            <label className='text-lg mb-2 flex'>Difficulty</label>
+            <Select
+              name='difficulty'
+              value={formik.values.difficulty}
+              onValueChange={onCustomChange('difficulty')}
+            >
+              <SelectTrigger>
+                <SelectValue placeholder='Select difficulty' />
+              </SelectTrigger>
+              <SelectContent>
+                <SelectItem value='easy'>Easy</SelectItem>
+                <SelectItem value='medium'>Medium</SelectItem>
+                <SelectItem value='hard'>Hard</SelectItem>
+              </SelectContent>
+            </Select>
+          </div>
+
+          <div className='mb-4'>
+            <label className='text-lg mb-2 flex'>Category</label>
+            <Select
+              name='category'
+              value={formik.values.category}
+              onValueChange={onCustomChange('category')}
+            >
+              <SelectTrigger>
+                <SelectValue placeholder='Select category' />
+              </SelectTrigger>
+              <SelectContent>
+                <SelectItem value='animals'>Animals</SelectItem>
+                <SelectItem value='countries'>Countries</SelectItem>
+                <SelectItem value='programming'>Programming</SelectItem>
+              </SelectContent>
+            </Select>
+          </div>
+
+          <div className='mb-4'>
+            <label className='text-lg mb-2 flex'>Number of words</label>
+            <Select
+              name='numberOfWords'
+              value={formik.values.numberOfWords}
+              onValueChange={onCustomChange('numberOfWords')}
+            >
+              <SelectTrigger>
+                <SelectValue placeholder='Select number of words' />
+              </SelectTrigger>
+              <SelectContent>
+                <SelectItem value='5'>5</SelectItem>
+                <SelectItem value='10'>10</SelectItem>
+                <SelectItem value='15'>15</SelectItem>
+              </SelectContent>
+            </Select>
+          </div>
+
+          <div className='mb-4'>
+            <label className='text-lg mb-2 flex'>Time limit</label>
+            <Select
+              name='timeLimit'
+              value={formik.values.timeLimit}
+              onValueChange={onCustomChange('timeLimit')}
+            >
+              <SelectTrigger>
+                <SelectValue placeholder='Select time limit' />
+              </SelectTrigger>
+              <SelectContent>
+                <SelectItem value='30'>30 seconds</SelectItem>
+                <SelectItem value='60'>1 minute</SelectItem>
+                <SelectItem value='90'>1 minute 30 seconds</SelectItem>
+              </SelectContent>
+            </Select>
+          </div>
+      </CardContent>
+
+      <CardFooter className='flex'>
+        <Button
+          size='lg'
+          className='w-full'
+          type='submit'
+          >
+          Start
+        </Button>
+      </CardFooter>
+          </form>
+    </Card>
+  );
+};
+
+export default SetupContainer;
