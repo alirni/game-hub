35d642ff0fdad3c9b0e8e188366642700a43507f
alirni <aliramezani19.1@gmail.com>
2025-02-22 23:03:37 +0330

    chore: remove

--- apps/word-guess/src/app/page.tsx
+++ apps/word-guess/src/app/page.tsx
@@ -1,12 +1,15 @@
 import { SetupContainer } from "../components";
+import GameSetupProvider from "../contexts/GameSetupContext";
 
 export default function Index() {
   return (
     <main className='flex flex-col items-center justify-center min-h-screen bg-gray-100'>
       <h1 className='text-6xl font-bold'>Word Guess</h1>
       <p className='text-2xl text-gray-500'>Guess the word</p>
-      
-      <SetupContainer />
+
+      <GameSetupProvider>
+        <SetupContainer />
+      </GameSetupProvider>
     </main>
   );
 }
